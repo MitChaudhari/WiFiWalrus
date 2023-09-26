b4600b142dac0f0758c27b0604446fa500748627
Mitansh Chaudhari <mitansh807@gmail.com>
2023-09-26 18:34:06 -0500

    Update mainwindow.py

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -16,20 +16,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.ui.scanButton.clicked.connect(self.scanNetworks)
         
     def scanNetworks(self):
-        self.ui.statusLabel.setText("Status: Scanning...")
-        networks = self.scanner.scan()
-        self.updateTable(networks)  # Update the table with the results received from network_scanner.py
-        self.ui.statusLabel.setText("Status: Idle")
+        try:
+            self.ui.statusLabel.setText("Status: Scanning...")
+            networks = self.scanner.scan()
+            print(networks)  # Debugging line to understand the structure of returned data
+            self.updateTable(networks)  # Update the table with the results received from network_scanner.py
+            self.ui.statusLabel.setText("Status: Idle")
+        except Exception as e:
+            self.ui.statusLabel.setText(f"Error: {str(e)}")
         
     def updateTable(self, networks):
         self.ui.tableWidget.setRowCount(len(networks))
         for i, network in enumerate(networks):
-            self.ui.tableWidget.setItem(i, 0, QTableWidgetItem(network['SSID']))
-            self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(network['Security']))
-            self.ui.tableWidget.setItem(i, 2, QTableWidgetItem(str(network['Score'])))
-            self.ui.tableWidget.setItem(i, 3, QTableWidgetItem(network['Recommendation']))
-            self.ui.tableWidget.setItem(i, 4, QTableWidgetItem(network['Detail']))
-    
+                if isinstance(network, dict):
+                    self.ui.tableWidget.setItem(i, 0, QTableWidgetItem(network.get('SSID', '')))
+                    self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(network.get('Security', '')))
+                    self.ui.tableWidget.setItem(i, 2, QTableWidgetItem(str(network.get('Score', ''))))
+                    self.ui.tableWidget.setItem(i, 3, QTableWidgetItem(network.get('Recommendation', '')))
+                    self.ui.tableWidget.setItem(i, 4, QTableWidgetItem(network.get('Detail', '')))
+                    
     def getNetworksFromBackend(self):
         # This method should interact with the backend to get the list of networks.
         # Placeholder for now.
