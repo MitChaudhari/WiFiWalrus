497a749d30f195636e31b767ecb90e93ef5188be
Mitansh Chaudhari <mitansh807@gmail.com>
2023-10-17 19:36:41 -0500

    update3

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,3 +1,5 @@
+# mainwindow.py
+
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from gui.designer import Ui_MainWindow
@@ -14,31 +16,29 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         
         # Connect the Scan button to the scanNetworks method
         self.ui.scanButton.clicked.connect(self.scanNetworks)
-        
-    # Inside your MainWindow class in mainwindow.py
 
-def scanNetworks(self):
-    try:
-        self.ui.statusLabel.setText("Status: Scanning...")
-        networks = self.scanner.scan()  # This should retrieve the list of networks
+    def scanNetworks(self):  # This method should be inside the MainWindow class
+        try:
+            self.ui.statusLabel.setText("Status: Scanning...")
+            networks = self.scanner.scan()  # This should retrieve the list of networks
 
-        # You can print 'networks' here to verify that it contains the expected data
-        print(networks)
+            print(networks)  # Debugging purpose
 
-        self.updateTable(networks)
-    except Exception as e:
-        print(f"An error occurred: {e}")  # This line will help catch any errors
-        self.ui.statusLabel.setText("Status: An error occurred while scanning.")
+            self.updateTable(networks)  # Update the table with the new data
+        except Exception as e:
+            print(f"An error occurred: {e}")  # This line will help catch any errors
+            self.ui.statusLabel.setText("Status: An error occurred while scanning.")
 
-# ...
+    def updateTable(self, networks):  # This method should also be inside the MainWindow class
+        self.ui.tableWidget.setRowCount(len(networks))
 
-def updateTable(self, networks):
-    self.ui.tableWidget.setRowCount(len(networks))
+        for i, network in enumerate(networks):
+            self.ui.tableWidget.setItem(i, 0, QTableWidgetItem(network['SSID']))
+            # Make sure your table has enough columns to set these items.
+            self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(network['BSSID']))
+            self.ui.tableWidget.setItem(i, 2, QTableWidgetItem(network['Security']))
 
-    for i, network in enumerate(networks):
-        self.ui.tableWidget.setItem(i, 0, QTableWidgetItem(network['SSID']))
-        self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(network['BSSID']))
-        self.ui.tableWidget.setItem(i, 2, QTableWidgetItem(network['Security']))
+# The rest of your main function remains unchanged.
 
 def main():
     app = QApplication(sys.argv)
