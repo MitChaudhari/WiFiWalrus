c1f7d94fbc9d586ce45605927bae6b39a6f4d16f
Mitansh Chaudhari <mitansh807@gmail.com>
2023-11-14 20:30:53 -0600

    update loading screen

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from PyQt5.QtCore import QThread, pyqtSignal
 from gui.designer import Ui_MainWindow
@@ -12,6 +13,40 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QLabel,
 from PyQt5.QtCore import QThread, pyqtSignal, QTimer
 from PyQt5.QtGui import QMovie
 from PyQt5.QtCore import QThread, pyqtSignal, QTimer, Qt  # Make sure Qt is included here
+from PyQt5.QtWidgets import QDialog, QVBoxLayout
+
+class LoadingDialog(QDialog):
+    def __init__(self, gif_path, parent=None):
+        super(LoadingDialog, self).__init__(parent)
+        self.setModal(True)
+        self.setWindowTitle("Loading...")
+
+        # Create a label to hold the GIF
+        self.gifLabel = QLabel()
+        movie = QMovie(gif_path)
+        self.gifLabel.setMovie(movie)
+        movie.start()
+
+        # Create a label for the text
+        self.textLabel = QLabel("Scanning the airwaves...")
+        self.textLabel.setAlignment(Qt.AlignCenter)
+        self.textLabel.setStyleSheet("color: white;")
+
+        # Set the dialog layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.gifLabel, alignment=Qt.AlignCenter)
+        layout.addWidget(self.textLabel)
+        self.setLayout(layout)
+
+        # Set the dialog's stylesheet
+        self.setStyleSheet("QDialog { background-color: black; }")
+
+        # Adjust the size of the QDialog to fit the content
+        self.adjustSize()
+
+    def update_text(self, text):
+        self.textLabel.setText(text)
+
 
 
 class ScannerWorker(QThread):
@@ -44,47 +79,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.updateProgressDialog)
         self.elapsedTime = 0
+        
+        gifPath = os.path.abspath("../assets/gif1.gif")  # Make sure this path is correct
+        self.loadingDialog = LoadingDialog(gifPath, self)
 
     def scanNetworks(self):
         self.ui.scanButton.setDisabled(True)
         self.ui.statusLabel.setText("Status: Scanning...")
-        self.showProgressDialog()  # Show the progress dialog
+        self.loadingDialog.show()  # Show the loading dialog
+        self.timer.start(1000)  # Start the timer to update the text
         self.scanner_thread.start()  # Start the scanner thread
-
-    def showProgressDialog(self):
-        
-        self.elapsedTime = 0
-        self.timer.start(1000)  # Update every second
-
-        # Assuming your structure is Front-End/gui/mainwindow.py and Front-End/assets/gif1.gif
-        gifPath = "../assets/gif1.gif"  # Correct path to your GIF
-        movie = QMovie(gifPath)
-
-        self.loadingLabel = QLabel(self.progressDialog)
-        self.loadingLabel.setAlignment(Qt.AlignCenter)  # Center the label
-        self.loadingLabel.setMovie(movie)
-        movie.start()
-
-        # Adjust the size of the QLabel to match your GIF's size (modify as needed)
-        self.loadingLabel.setFixedSize(200, 200)  # Update with the actual size of your GIF
-
-        # Set the layout of the progress dialog to include the loading label
-        self.progressDialog.setLabel(self.loadingLabel)
-        self.progressDialog.show()
 
     def updateProgressDialog(self):
         phrases = ["Scanning the airwaves...", "Almost there...", "Hang tight, we're scanning...", "Just a moment..."]
         current_phrase = phrases[self.elapsedTime % len(phrases)]
-        self.progressDialog.setLabelText(f"{current_phrase} ({self.elapsedTime} seconds elapsed)")
+        self.loadingDialog.update_text(current_phrase)  # Update the text in the loading dialog
         self.elapsedTime += 1
     
     def onScanComplete(self, networks):
         self.updateTable(networks)
         db_status = self.db_manager.send_to_database(networks)
         self.ui.statusLabel.setText(f"Status: {db_status}")
-        self.progressDialog.accept()  # Explicitly accept/close the dialog
+        self.loadingDialog.accept()  # Close the loading dialog
         self.timer.stop()  # Stop the timer
-        self.ui.scanButton.setDisabled(False)  # Re-enable the scan button
+        self.ui.scanButton.setDisabled(False)  
 
     def updateTable(self, networks):
         self.ui.tableWidget.setRowCount(len(networks))
